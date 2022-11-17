7fc87914fa91cb8b9bdabcd8c00ff4778b32c8a6
Philippine Berton <Phi.berton@hotmail.fr>
2022-11-17 08:22:26 +0100

    choose a city

--- project/js/main.js
+++ project/js/main.js
@@ -1,12 +1,19 @@
-$(document).ready(function(){
-  getWeather();
-});
+// $(document).ready(function(){
+//   getWeather();
+// });
 
-function getWeather() {
- var url = "https://api.openweathermap.org/data/2.5/weather?q=Nancy&units=metric&APPID="+apiKey;
+function getWeather(searchQuery) {
+ var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=metric&APPID="+apiKey;
 
  $.ajax(url,{success: function(data){
-  $(".city").text(data.name);
+  $(".city").text();
   $(".temp").text(data.main.temp);
+ }, error: function(error){
+  $(".error-message").text("An error occured");
  }})
 };
+
+function searchWeather() {
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
+}
